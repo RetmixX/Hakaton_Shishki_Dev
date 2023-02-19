0404ba5dcc5a9200136bffe45ab9090015bf3e4f
Kirill Chernogrivov <kirill.2201@inbox.ru>
2023-02-19 13:38:05 +0700

    leveling

--- frontend/hakaton-front/src/pages/ProfilePage/components/PersonCard/index.vue
+++ frontend/hakaton-front/src/pages/ProfilePage/components/PersonCard/index.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="person-card">
-    <div class="person-card__image"></div>
+    <div
+      v-if="person.employee?.lvl || person.hr_profile?.middleName"
+      class="person-card__image"
+      :style="{
+        'background-image': `url(../../../src/assets/images/profilePage/personLevel${person.employee?.lvl}.png`,
+      }"
+    ></div>
     <div class="person-card__information">
       <div class="person-card__name">
-        {{ person.employee?.name ? person.employee?.name : person.hr_profile?.firstName }}<br>
-        {{ person.employee?.middleName ? person.employee?.middleName : person.hr_profile?.middleName }}
+        {{
+          person.employee?.name
+            ? person.employee?.name
+            : person.hr_profile?.firstName
+        }}<br />
+        {{
+          person.employee?.middleName
+            ? person.employee?.middleName
+            : person.hr_profile?.middleName
+        }}
       </div>
       <div class="person-card__join-date">
-        <p class="person-card--label">Начало работы: </p>
-        {{ person.employee?.workStart ? person.employee?.workStart : person.hr_profile?.startWork }}
+        <p class="person-card--label">Начало работы:</p>
+        {{
+          person.employee?.workStart
+            ? person.employee?.workStart
+            : person.hr_profile?.startWork
+        }}
       </div>
       <div class="person-card__department" v-if="person.employee?.division">
-        <p class="person-card--label">Отдел: </p>
+        <p class="person-card--label">Отдел:</p>
         {{ person.employee?.division }}
       </div>
       <div class="person-card__speciality">
-        <p class="person-card--label">Специльность: </p>
+        <p class="person-card--label">Специльность:</p>
         {{ person.employee?.rang ? person.employee?.rang : "HR" }}
       </div>
     </div>
